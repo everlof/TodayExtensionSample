b73ea83bc05e0e3fa6b037bb1b7fbc5a17383759
David Everlof <everlof@gmail.com>
2017-01-07 18:00:52 +0100

    Initial commit

---
+++ TodayExtension/TodayViewController.swift
@@ -0,0 +1,61 @@
+//
+//  TodayViewController.swift
+//  TodayExtension
+//
+//  Created by David Everlöf on 2017-01-07.
+//  Copyright © 2017 David Everlöf. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+
+    let lbl = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        lbl.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae tempor nulla, in volutpat lectus. Sed quis orci sit amet velit cursus congue non accumsan turpis. Phasellus quis augue lobortis, pharetra arcu vitae, condimentum nunc. Nam rutrum massa ac feugiat eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non erat orci. Ut consequat faucibus sapien, et luctus magna posuere tempor."
+        lbl.numberOfLines = 0
+        lbl.backgroundColor = .blue
+        
+        view.addSubview(lbl)
+        
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        lbl.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 18).isActive = true
+        lbl.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -18).isActive = true
+        
+        lbl.topAnchor.constraint(equalTo: view.topAnchor, constant: 18).isActive = true
+        lbl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -18).isActive = true
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        lbl.preferredMaxLayoutWidth = 300
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
---
+++ TodayExtensionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodayExtensionSample
+//
+//  Created by David Everlöf on 2017-01-07.
+//  Copyright © 2017 David Everlöf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
